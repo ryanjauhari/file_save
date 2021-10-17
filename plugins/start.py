115397dc05dde5e3e96c6a9371f01ab2d98eeb51
RyanID <87651445+ryanjauhari@users.noreply.github.com>
2021-10-17 07:57:23 +0700

    apus tag i
    Terdapat kode tag yang membuat bot stoped

--- plugins/start.py
+++ plugins/start.py
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>❌ Konten dilindungi</b>\n\n<i>Sebelum mengakses konten wajib berlangganan ke channel saya terlebih dahulu."
+    text = "<b>❌ Konten dilindungi</b>\n\nSebelum mengakses konten wajib berlangganan ke channel saya terlebih dahulu."
     message_text = message.text
     try:
         command, argument = message_text.split()
